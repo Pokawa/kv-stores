b67a368e3eca8d0edc0298789adb3378f621d9a6
Hubert Cwajda <cwajdahubert@gmail.com>
2022-03-06 17:08:56 +0100

    restructure of cmake project

---
+++ include/kvstores/dummy_header.hpp
@@ -0,0 +1,6 @@
+#ifndef KVSTORES_DUMMY_HEADER_HPP
+#define KVSTORES_DUMMY_HEADER_HPP
+auto fun(auto a, auto b) {
+    return a + b;
+}
+#endif  // KVSTORES_DUMMY_HEADER_HPP
--- test/dummy_test.cpp
+++ test/dummy_test.cpp
@@ -1,5 +1,10 @@
 #include <gtest/gtest.h>
+#include <kvstores/dummy_header.hpp>
 
 TEST(dummy_test_suite, test1) {
     ASSERT_EQ(1 + 2, 3);
+}
+
+TEST(dummy_test_suite, dummy_header_test) {
+
 }
