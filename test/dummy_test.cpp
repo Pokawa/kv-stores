6e9ca357dced61b90208624b06323bd07e2bc08f
Hubert Cwajda <cwajdahubert@gmail.com>
2022-03-06 17:14:52 +0100

    formatting

--- test/dummy_test.cpp
+++ test/dummy_test.cpp
@@ -1,10 +1,10 @@
 #include <gtest/gtest.h>
+
 #include <kvstores/dummy_header.hpp>
 
-TEST(dummy_test_suite, test1) {
-    ASSERT_EQ(1 + 2, 3);
-}
+TEST(dummy_test_suite, test1) { ASSERT_EQ(1 + 2, 3); }
 
 TEST(dummy_test_suite, dummy_header_test) {
-
+    auto result = fun(1, 2);
+    ASSERT_EQ(result, 3);
 }
