2835bb1fbbafa679cd4a1e6873a55b587d3b0a7f
Hubert Cwajda <cwajdahubert@gmail.com>
2022-03-06 17:13:02 +0100

    formatting

--- include/kvstores/dummy_header.hpp
+++ include/kvstores/dummy_header.hpp
@@ -1,6 +1,4 @@
 #ifndef KVSTORES_DUMMY_HEADER_HPP
 #define KVSTORES_DUMMY_HEADER_HPP
-auto fun(auto a, auto b) {
-    return a + b;
-}
+auto fun(auto a, auto b) { return a + b; }
 #endif  // KVSTORES_DUMMY_HEADER_HPP
